4d06c34bac3cfb96740f314667a53b648755b613
Tyler <Tyler@service84.io>
2025-08-06 13:26:53 -0400

    Use the full CTCode CPPTranspiler

--- build_full.sh
+++ build_full.sh
@@ -3,8 +3,8 @@
 tower S84.CTCode.dbnf DSA::Vent::Tower::CPPGenerator
 tower S84.CTCode.dbnf DSA::Vent::Tower::CTCodeGeneratorBoot
 
-ctcode S84.CTCode.CPPTranspiler.ctcode s84::ctcode::CPPTranspiler
-ctcode S84.CTCode.CPPTranspilerBoot.ctcode s84::ctcode::CPPTranspiler
-ctcode S84.CTCode.dbnf.ctcode s84::ctcode::CPPTranspiler
+ctcode S84.CTCode.CPPTranspiler.ctcode s84::ctcode::CPPTranspilerBoot
+ctcode S84.CTCode.CPPTranspilerBoot.ctcode s84::ctcode::CPPTranspilerBoot
+ctcode S84.CTCode.dbnf.ctcode s84::ctcode::CPPTranspilerBoot
 
 $(dirname $0)/build_source.sh
